eb46e0e8388c838f8ba536254c9b6b9d8034b2b1
Denis Orekhov <3008denis@gmail.com>
2024-06-06 13:47:14 +0500

    Add code to App.java
    Add methods for creating and working with Javalin

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,24 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello, World"));
+        return app;
+    }
+
+    public static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        return Integer.parseInt(port);
     }
 
     public static int tempMet() {
