20e9108b7aa2db6a42ad1fb9255f0658ccf97023
Denis Orekhov <3008denis@gmail.com>
2024-06-06 13:31:37 +0500

    Add javalin and SLF4J dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("io.javalin:javalin:6.1.3")
 }
 
 tasks.test {
