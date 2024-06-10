5439c218df8495f5424fb17537c82b112dafef39
Denis Orekhov <3008denis@gmail.com>
2024-06-10 13:26:21 +0500

    Add allArgsConstr to Site.java

--- app/src/main/java/hexlet/code/model/Site.java
+++ app/src/main/java/hexlet/code/model/Site.java
@@ -1,5 +1,6 @@
 package hexlet.code.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Site {
     private Long id;
     private String name;
