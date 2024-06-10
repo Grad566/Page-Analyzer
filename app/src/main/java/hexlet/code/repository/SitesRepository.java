552ee8c99d200d321b792cf098afe36677682c3f
Denis Orekhov <3008denis@gmail.com>
2024-06-10 13:28:44 +0500

    Add method getSites in SiteRepository.java

--- app/src/main/java/hexlet/code/repository/SitesRepository.java
+++ app/src/main/java/hexlet/code/repository/SitesRepository.java
@@ -1,2 +1,48 @@
-package hexlet.code.repository;public class SitesRepository {
+package hexlet.code.repository;
+
+import hexlet.code.model.Site;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class SitesRepository extends BaseRepository{
+    public static void save(Site site) throws SQLException {
+        var sql = "INSERT INTO sites (name) VALUES (?)";
+        try (var conn = dataSource.getConnection();
+                var prepareStmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            prepareStmt.setString(1, site.getName());
+            prepareStmt.executeUpdate();
+
+            var generatedKey = prepareStmt.getGeneratedKeys();
+            if (generatedKey.next()) {
+                site.setId(generatedKey.getLong(1));
+                site.setCreatedAt(generatedKey.getString("created_at"));
+            } else {
+                throw new SQLException("Страница уже существует");
+            }
+        }
+    }
+
+    public static List<Site> getSites() throws SQLException {
+        var sql = "SELECT * FROM sites";
+        try (var conn = dataSource.getConnection();
+                var stmt = conn.prepareStatement(sql)) {
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<Site>();
+            while (resultSet.next()) {
+                var id = resultSet.getString("id");
+                var name = resultSet.getString("name");
+                var created_at = resultSet.getString("created_at");
+                var site = new Site(Long.parseLong(id), name, created_at);
+                result.add(site);
+            }
+            return result;
+        }
+    }
 }
